9fe7276db4a088b7d8185ef16928cdfceac56146
Сергей Николаевич <tukmachev.18@yandex.ru>
2022-11-04 22:42:06 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class
 Main {
     public static void main(String[] args) {
-// Не разобрался, как использовать метод из условий домашки
 // Задача 1
     System.out.println("Задача 1");
     int sum = 0;
-    int[] paymentAmount = {121_000, 122_000, 123_000, 124_000, 125_000, 126_000, 127_000, 128_000, 129_000, 120_000, 131_000, 132_000, 133_000, 134_000, 135_000, 136_000, 137_000, 138_000, 139_000, 130_000, 141_000, 142_000, 143_000, 144_000, 145_000, 146_000, 147_000, 148_000, 149_000, 140_000};
+    int[] paymentAmount = new int[30];
     for (int i = 0; i < paymentAmount.length; i++) {
-    sum += paymentAmount[i];
-    }
-    System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        paymentAmount[i] = ((int)(Math.random() * 100_000) + 100_000);
+        sum += paymentAmount[i];}
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 // Задача 2
 System.out.println("Задача 2");
     int maxSum = 0;
